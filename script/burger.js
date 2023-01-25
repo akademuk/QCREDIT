aa4a1a0e387f58101ea468f5b621b49f3da1c492
akademuk <akademuk24@gmail.com>
2023-01-25 17:24:34 +0200

    ss

--- script/burger.js
+++ script/burger.js
@@ -1,7 +1,9 @@
 $('.openBurger').click(function() {
   $('.menu').addClass('open_menu');
+  $('.body').addClass('active');
 });
 
 $('.close').click(function() {
   $('.menu').removeClass('open_menu');
+  $('.body').removeClass('active');
 });
